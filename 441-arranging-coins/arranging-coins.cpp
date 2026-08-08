f658c60cdda045f63a87a799a220826b50905c6a
sarthak mathur <mathursarthak750@gmail.com>
2026-08-08 12:09:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (68.68%) - LeetSync

--- 441-arranging-coins/arranging-coins.cpp
+++ 441-arranging-coins/arranging-coins.cpp
@@ -1,7 +1,26 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int count =0;
+        int left = 0 , right = n;
+        while ( left <= right ){
+            long long  mid = (left + right) /2;
+            long long  coins = mid *(mid +1)/2;
+
+            if (coins<=n){
+            left = mid +1;
+            }
+            else {
+                right = mid -1 ; 
+            }
+
+         } 
+         return right;
+
+
+    }
+    
+    };
+       /* int count =0;
         
 
         for (int i= 1;i <= n;i++){
@@ -22,5 +41,5 @@ public:
 
         }
         return count ;
-    }
-};
+    */
+    

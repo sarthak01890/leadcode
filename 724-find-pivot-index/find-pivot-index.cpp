c800639cb5645773754996c823ce0cbb3692b7bf
sarthak mathur <mathursarthak750@gmail.com>
2026-08-14 20:42:30 +0530

    Time: 0 ms (100.00%) | Memory: 35.6 MB (79.60%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -2,24 +2,45 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
          
-         int n= nums.size();
-         vector <int> prefix(n,0);
-         for (int i= 1 ;i< n ;i++){
-            prefix[i]= prefix[i-1] + nums[i-1];
-         }
-         vector <int> suffix(n,0);
-         for (int i = n -2  ;  i>=0 ; i--){
-            suffix[i]= suffix[i+1]+ nums[i+1];
-         }
-         for (int i = 0; i < n; i++){
-         if (prefix[i]== suffix[i]){
-            return i;
-         }
+       /*     int n= nums.size();
+            vector <int> prefix(n,0);
+            for (int i= 1 ;i< n ;i++){
+                prefix[i]= prefix[i-1] + nums[i-1];
+            }
+            vector <int> suffix(n,0);
+            for (int i = n -2  ;  i>=0 ; i--){
+                suffix[i]= suffix[i+1]+ nums[i+1];
+            }
+            for (int i = 0; i < n; i++){
+            if (prefix[i]== suffix[i]){
+                return i;
+            }
 
+            }
+            
+            return -1;
+         */
+
+
+         int n = nums.size();
+         int sum = 0;
+          for (int i = 0 ; i< n ; i++){
+            sum += nums[i];
+          }
+         int left = 0;
+         int right ;
+         for (int i = 0 ;  i < n ; i++){
+            
+            right = sum - left - nums[i];
+            if ( left == right ){
+                return i;
+            }
+            left += nums[ i];
+             
+            
          }
-         
-        return -1;
-         
+         return -1;
+        
         
     }
 };
